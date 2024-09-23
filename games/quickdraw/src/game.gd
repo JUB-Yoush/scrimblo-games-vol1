7fc36d2a705ad319992fe91d32e36521987d67a4
JUB_Yoush <yoush1337@gmail.com>
2024-09-22 21:50:35 -0400

    dude gdscript is cheating

--- games/quickdraw/src/game.gd
+++ games/quickdraw/src/game.gd
@@ -1,11 +1,59 @@
 extends Node2D
+# game:
+# countdown from 3, draw appears
+# have to press the button after draw appears
+# press to early/late, lose
 
+signal game_over(result)
 
-# Called when the node enters the scene tree for the first time.
+enum GAME_STATE {SETUP,DRAWING,DRAWN,ROUND_END,DONE}
+
+var current_game_state = GAME_STATE.SETUP
+var countdown_sprite := ""
+var level = 0
+var player_shot:bool
+const DRAW_SPEEDS:Array[float] = [1,0.5,0.25]
+
+const DRAW_VARIANCE:float = 5.0
+var rng = RandomNumberGenerator.new()
+@onready var countdown:Sprite2D = $Countdown
+
 func _ready() -> void:
+	play_round()
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func lose():
+	game_over.emit(false)
 	pass
+
+func shoot():
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("draw"):
+		if current_game_state == GAME_STATE.DRAWING:
+			lose()
+			pass
+		elif current_game_state == GAME_STATE.DRAWN:
+			shoot()
+	pass
+
+func play_round():
+	current_game_state = GAME_STATE.DRAWING
+	# render 3,2,1 then make draw appear, with some vairance
+	countdown.texture = load("res://games/quickdraw/assets/temp-timer1.png")
+	await get_tree().create_timer(1).timeout
+	countdown.texture = load("res://games/quickdraw/assets/temp-timer2.png")
+	await get_tree().create_timer(1).timeout
+	countdown.texture = load("res://games/quickdraw/assets/temp-timer3.png")
+	var extra_time = 2 + rng.randf_range(-DRAW_VARIANCE, DRAW_VARIANCE)
+	await get_tree().create_timer(extra_time).timeout
+	draw()
+
+func draw():
+	current_game_state = GAME_STATE.DRAWN
+	await get_tree().create_timer(DRAW_SPEEDS[level]).timeout
+	if not player_shot:
+		lose()
+
+
