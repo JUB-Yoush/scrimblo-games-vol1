a7d18322a896c5238e155c3d8a939db5bad89877
JUB_Yoush <yoush1337@gmail.com>
2024-10-11 22:48:06 -0400

    making the menu usable

--- games/vsKayla/game.gd
+++ games/vsKayla/game.gd
@@ -2,7 +2,23 @@ extends Node2D
 
 # set up game states
 enum GAME_STATE {SETUP,MENUS,DANMAKU,ENDED}
+enum COMMANDS {FIGHT,ACT,ITEM,MERCY}
+
+var items = ["Bingus","M.Stew"]
 
 var current_game_state:GAME_STATE = GAME_STATE.SETUP
 
 var hp = 20
+@onready var actionButtons = $Control/VBoxContainer/ActionMenuMargin/ActionButtons
+
+
+func _ready() -> void:
+    # the heart can appear on the buttons when focused
+    %FightButton.grab_focus()
+    %FightButton.pressed.connect(command_selected,COMMANDS.FIGHT)
+
+func command_selected(command):
+    if command == COMMANDS.FIGHT:
+        #render the kayla option, same w mercy
+        pass
+    pass
