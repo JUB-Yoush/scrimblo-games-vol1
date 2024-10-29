b1d01ea3e81ef5c521d6995d7ada8d3ac355a476
JUB_Yoush <yoush1337@gmail.com>
2024-10-28 22:22:51 -0400

    its peak...

--- games/vsKayla/danmaku.gd
+++ games/vsKayla/danmaku.gd
@@ -7,74 +7,76 @@ var attackTimer = Timer.new()
 signal attack_over
 
 func _ready():
-    attackTimer.one_shot = true
-    add_child(attackTimer)
-    attackTimer.timeout.connect(end)
+	attackTimer.one_shot = true
+	add_child(attackTimer)
+	attackTimer.timeout.connect(end)
 
 
 func start(enemy_text):
-    %Heart.global_position = Vector2(214,128)
-    visible = true
-    %Heart.can_move = false
-    %MenuCommands.visible = false
-    if enemy_text != "":
-        %Dialog.visible = true
-        %Dialog.text = enemy_text
-        await get_tree().create_timer(1).timeout
-        %Dialog.visible = false
-    %Heart.can_move = true
-    pattern1.call()
-    # find a more natural way to do this
-    attackTimer.start(attack_time)
+	%MenuBox.visible = false
+	%Heart.global_position = Vector2(214,128)
+	visible = true
+	%Heart.can_move = false
+	%MenuCommands.visible = false
+	if enemy_text != "":
+		%Dialog.visible = true
+		%Dialog.text = enemy_text
+		await get_tree().create_timer(1).timeout
+		%Dialog.visible = false
+	%Heart.can_move = true
+	pattern1.call()
+	# find a more natural way to do this
+	attackTimer.start(attack_time)
 
 
 func end():
-    for bullet in get_tree().get_nodes_in_group("bullets"):
-        bullet.queue_free()
-    visible = false
-    %MenuCommands.visible = true
-    attack_over.emit()
+	%MenuBox.visible = true
+	for bullet in get_tree().get_nodes_in_group("bullets"):
+		bullet.queue_free()
+	visible = false
+	%MenuCommands.visible = true
+	attack_over.emit()
 
 
 func enemy_says(text):
-    %Dialog.visible = true
-    %Dialog.text = text
-    await get_tree().create_timer(1).timeout
-    %Dialog.visible = false
+	%Dialog.visible = true
+	%Dialog.text = text
+	await get_tree().create_timer(1).timeout
+	%Dialog.visible = false
 
 
 var pattern1 = func():
-    # drop from top of screen, at some point detonate and create spreads of 3 bullets to dodge
-    # harder version: spread of 5
-    var dropScene :PackedScene= load("res://games/vsKayla/bullet_patterns/drops.tscn")
-    # get range that isn't above the heart box
-    for i in range(5):
-        var spawn_pos_x = randi_range(70,350)
-        while spawn_pos_x > 150 and spawn_pos_x < 270:
-            spawn_pos_x = randi_range(70,350)
-        var drop = dropScene.instantiate()
-        drop.global_position = Vector2(spawn_pos_x,0)
-        add_child(drop)
-        await get_tree().create_timer(.5).timeout
+	# drop from top of screen, at some point detonate and create spreads of 3 bullets to dodge
+	# harder version: spread of 5
+	var dropScene :PackedScene= load("res://games/vsKayla/bullet_patterns/drops.tscn")
+	# get range that isn't above the heart box
+	for i in range(5):
+		var spawn_pos_x = randi_range(70,350)
+		while spawn_pos_x > 150 and spawn_pos_x < 270:
+			spawn_pos_x = randi_range(70,350)
+		var drop = dropScene.instantiate()
+		drop.global_position = Vector2(spawn_pos_x,0)
+		add_child(drop)
+		await get_tree().create_timer(.5).timeout
 
 
 func pattern2():
-    # zebra runs across the screen and the dust it kicks up rains down
-    #harder version: more dust
-    pass
+	# zebra runs across the screen and the dust it kicks up rains down
+	#harder version: more dust
+	pass
 
 func pattern3():
-    # full screen spiral
-    pass
+	# full screen spiral
+	pass
 
 func pattern4():
 
-    pass
+	pass
 
 func pattern5():
-    # full screen spiral
-    pass
+	# full screen spiral
+	pass
 
 func pattern6():
-    # full screen spiral
-    pass
+	# full screen spiral
+	pass
--- games/vsKayla/game.gd
+++ games/vsKayla/game.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 # set up game states
 enum GAME_STATE {SETUP,MENUS,DANMAKU,ENDED}
@@ -28,29 +28,29 @@ var sparable = false
 @onready var danmaku = $Danmaku
 
 var social_credit = 0:
-    set(value):
-        social_credit = value
-        %SocialCreditLabel.text = str(social_credit) # add leading 0s later
-        if social_credit == goal_credit:
-            credit_reached()
+	set(value):
+		social_credit = value
+		%SCreditBar.value = value
+		if social_credit == goal_credit:
+			credit_reached()
 
 var goal_credit = 100
 var turn_count = 1:
-    set(value):
-        turn_count = value
-        if turn_count == deadline:
-            game_over()
-            pass
+	set(value):
+		turn_count = value
+		if turn_count == deadline:
+			game_over()
+			pass
 var deadline = 20
 
 var max_hp := 20
 var hp := max_hp:
-    set(value):
-        hp =  min(value, max_hp)
-        %HPBar.value = hp
-        if hp <= 0:
-            game_over()
-        %HpLabel.text = str(hp) +"/"+ str(max_hp)
+	set(value):
+		hp =  min(value, max_hp)
+		%HPBar.value = hp
+		if hp <= 0:
+			game_over()
+		%HpLabel.text = str(hp) +"/"+ str(max_hp)
 
 
 var current_game_state:GAME_STATE = GAME_STATE.SETUP
@@ -60,200 +60,200 @@ signal txb_adv
 signal txb_back
 
 func _ready() -> void:
-    get_viewport().gui_focus_changed.connect(update_cursor)
-    danmaku.attack_over.connect(func(): update_game_state(GAME_STATE.MENUS))
-    clear_menu()
-    %FightButton.pressed.connect( func(): command_selected(COMMANDS.FIGHT))
-    %ActButton.pressed.connect(func(): command_selected(COMMANDS.ACT))
-    %ItemButton.pressed.connect(func(): command_selected(COMMANDS.ITEM))
-    %MercyButton.pressed.connect(func(): command_selected(COMMANDS.MERCY))
-    start_turn()
+	get_viewport().gui_focus_changed.connect(update_cursor)
+	danmaku.attack_over.connect(func(): update_game_state(GAME_STATE.MENUS))
+	clear_menu()
+	%FightButton.pressed.connect( func(): command_selected(COMMANDS.FIGHT))
+	%ActButton.pressed.connect(func(): command_selected(COMMANDS.ACT))
+	%ItemButton.pressed.connect(func(): command_selected(COMMANDS.ITEM))
+	%MercyButton.pressed.connect(func(): command_selected(COMMANDS.MERCY))
+	start_turn()
 
 func update_cursor(control:Control):
-    %Cursor.visible = true
-    %Cursor.global_position = control.global_position
-    %Cursor.position.x += 10
-    %Cursor.position.y += 12
+	%Cursor.visible = true
+	%Cursor.global_position = control.global_position
+	%Cursor.position.x += 10
+	%Cursor.position.y += 12
 
 func update_game_state(new_state:GAME_STATE):
-    print("update_game_state: ",current_game_state)
-    # unload prev state
-    if current_game_state == GAME_STATE.MENUS:
-        clear_menu()
-        toggle_action_buttons(0)
+	print("update_game_state: ",current_game_state)
+	# unload prev state
+	if current_game_state == GAME_STATE.MENUS:
+		clear_menu()
+		toggle_action_buttons(0)
 
-    # load new state
-    if new_state == GAME_STATE.DANMAKU:
-        danmaku.start(enemy_turn_text)
-    if new_state == GAME_STATE.MENUS:
-        turn_count += 1
-        start_turn()
-    current_game_state = new_state
+	# load new state
+	if new_state == GAME_STATE.DANMAKU:
+		danmaku.start(enemy_turn_text)
+	if new_state == GAME_STATE.MENUS:
+		turn_count += 1
+		start_turn()
+	current_game_state = new_state
 
 func start_turn():
-    menu_state = MENU_STATES.COMMANDS
-    # render the menu and the action buttons
-    clear_menu()
-    print_to_menu(start_turn_text)
-    toggle_action_buttons(2)
-    %FightButton.grab_focus()
-    update_cursor(%FightButton)
-    await txb_back
-    if menu_state != MENU_STATES.CHOSEN:
-        clear_menu()
-        start_turn()
+	menu_state = MENU_STATES.COMMANDS
+	# render the menu and the action buttons
+	clear_menu()
+	print_to_menu(start_turn_text)
+	toggle_action_buttons(2)
+	%FightButton.grab_focus()
+	update_cursor(%FightButton)
+	await txb_back
+	if menu_state != MENU_STATES.CHOSEN:
+		clear_menu()
+		start_turn()
 
 func command_selected(command):
-    menu_state = MENU_STATES.MENU
-    # going backwards in menus should happen
-    awaiting_command = true
-    toggle_action_buttons(0)
-    clear_menu()
-    if command == COMMANDS.FIGHT:
-        %Cmd1.visible = true
-        %Cmd1.text = "    *Kayla"
-        %Cmd1.pressed.connect(fight)
-    elif command == COMMANDS.ACT:
-        populate_acts()
-    elif command == COMMANDS.MERCY:
-        %Cmd1.visible = true
-        %Cmd1.text = "    *Kayla"
+	menu_state = MENU_STATES.MENU
+	# going backwards in menus should happen
+	awaiting_command = true
+	toggle_action_buttons(0)
+	clear_menu()
+	if command == COMMANDS.FIGHT:
+		%Cmd1.visible = true
+		%Cmd1.text = "    *Kayla"
+		%Cmd1.pressed.connect(fight)
+	elif command == COMMANDS.ACT:
+		populate_acts()
+	elif command == COMMANDS.MERCY:
+		%Cmd1.visible = true
+		%Cmd1.text = "    *Kayla"
 
-        %Cmd1.pressed.connect(fight)
-    elif command == COMMANDS.ITEM:
-        populate_items(0)
+		%Cmd1.pressed.connect(fight)
+	elif command == COMMANDS.ITEM:
+		populate_items(0)
 
-    %Cmd1.grab_focus()
+	%Cmd1.grab_focus()
 
-    await command_completed
-    awaiting_command = false
-    clear_menu()
-    end_turn()
+	await command_completed
+	awaiting_command = false
+	clear_menu()
+	end_turn()
 
 func fight():
-    menu_state = MENU_STATES.CHOSEN
-    # do the timing minigame
-    print('this is where youd do the timing game shell dodge every time so who even cares')
-    # await or whatever
-    command_completed.emit()
-    pass
+	menu_state = MENU_STATES.CHOSEN
+	# do the timing minigame
+	print('this is where youd do the timing game shell dodge every time so who even cares')
+	# await or whatever
+	command_completed.emit()
+	pass
 
 func clear_menu():
-    %Cursor.visible = false
-    %Dialog.text = ""
-    %MenuString.text = ""
-    for box in cmdboxes:
-        box.visible = false
-        box.text = ""
-        # reset signals
-        for sig in box.get_signal_connection_list("pressed"):
-            box.pressed.disconnect(sig["callable"])
-            pass
+	%Cursor.visible = false
+	%Dialog.text = ""
+	%MenuString.text = ""
+	for box in cmdboxes:
+		box.visible = false
+		box.text = ""
+		# reset signals
+		for sig in box.get_signal_connection_list("pressed"):
+			box.pressed.disconnect(sig["callable"])
+			pass
 
 func toggle_action_buttons(state):
-    for btn in action_buttons:
-        btn.focus_mode = state
+	for btn in action_buttons:
+		btn.focus_mode = state
 
 func use_item(item_str):
 
-    menu_state = MENU_STATES.CHOSEN
-    clear_menu()
-    if item_str == "*M.Stew":
-        hp += 20
-        print_to_menu("You drank Stew sent by Maddie \n Healed 20 HP")
-        await txb_adv
-    elif item_str == "*Bingus":
-        hp += 10
-        print_to_menu("It's kinda melted. \n Healed 20 HP")
-        await txb_adv
-    elif item_str == "*Scrimbookie":
-        scrimbookied = true
-        print_to_menu("You feel especially persuasive.\n Boosted Appeal power for 3 turns")
-        await txb_adv
-    command_completed.emit()
+	menu_state = MENU_STATES.CHOSEN
+	clear_menu()
+	if item_str == "*M.Stew":
+		hp += 20
+		print_to_menu("You drank Stew sent by Maddie \n Healed 20 HP")
+		await txb_adv
+	elif item_str == "*Bingus":
+		hp += 10
+		print_to_menu("It's kinda melted. \n Healed 20 HP")
+		await txb_adv
+	elif item_str == "*Scrimbookie":
+		scrimbookied = true
+		print_to_menu("You feel especially persuasive.\n Boosted Appeal power for 3 turns")
+		await txb_adv
+	command_completed.emit()
 
 func use_act(act_str):
-    menu_state = MENU_STATES.CHOSEN
-    if act_str == "*Check":
-        print_to_menu("Kayla: 1HP, 1ATK, 1DEF \n She runs lassonde clubs.")
-        await txb_adv
+	menu_state = MENU_STATES.CHOSEN
+	if act_str == "*Check":
+		print_to_menu("Kayla: 1HP, 1ATK, 1DEF \n She runs lassonde clubs.")
+		await txb_adv
 
-    elif act_str == "*Appeal":
-        print_to_menu(appeals[0])
-        social_credit += 5
-        await txb_adv
+	elif act_str == "*Appeal":
+		print_to_menu(appeals[0])
+		social_credit += 5
+		await txb_adv
 
-    elif act_str == "*Oppose":
-        if opposing:
-            print('guh')
-        else:
-            opposing = true
-            print_to_menu(opposes[0])
-            await txb_adv
-            start_turn_text = "kayla is feeling particularly critical of your club"
+	elif act_str == "*Oppose":
+		if opposing:
+			print('guh')
+		else:
+			opposing = true
+			print_to_menu(opposes[0])
+			await txb_adv
+			start_turn_text = "kayla is feeling particularly critical of your club"
 
-    elif act_str == "*HR Talk":
-        textprompt(hrtalks[0])
-        if opposing:
-            opposing = false
-            print_to_menu("Situation Diffused!")
-            await txb_adv
-            social_credit += 15
-            start_turn_text = "scrimblo permiates the air"
-        else:
-            textprompt("Kayla looks indifferently")
+	elif act_str == "*HR Talk":
+		textprompt(hrtalks[0])
+		if opposing:
+			opposing = false
+			print_to_menu("Situation Diffused!")
+			await txb_adv
+			social_credit += 15
+			start_turn_text = "scrimblo permiates the air"
+		else:
+			textprompt("Kayla looks indifferently")
 
 
-    print('using action '+ act_str)
-    command_completed.emit()
+	print('using action '+ act_str)
+	command_completed.emit()
 
 
 
 # the offset is the 4 items rendered
 func populate_items(offset):
-    offset = offset * 4
-    for i in range(4):
-        curr_items.append(items[offset + i])
-        cmdboxes[i].visible = true
-        cmdboxes[i].pressed.connect(func(): use_item(curr_items[i]))
-        cmdboxes[i].text = "    " + curr_items[i]
+	offset = offset * 4
+	for i in range(4):
+		curr_items.append(items[offset + i])
+		cmdboxes[i].visible = true
+		cmdboxes[i].pressed.connect(func(): use_item(curr_items[i]))
+		cmdboxes[i].text = "    " + curr_items[i]
 
 func populate_acts():
-    for i in range(4):
-        cmdboxes[i].visible = true
-        cmdboxes[i].pressed.connect(func(): use_act(acts[i]))
-        cmdboxes[i].text = "    " +acts[i]
-    pass
+	for i in range(4):
+		cmdboxes[i].visible = true
+		cmdboxes[i].pressed.connect(func(): use_act(acts[i]))
+		cmdboxes[i].text = "    " +acts[i]
+	pass
 
 
 func end_turn():
-    update_game_state(GAME_STATE.DANMAKU)
+	update_game_state(GAME_STATE.DANMAKU)
 
 func textprompt(text):
-    print_to_menu(text)
-    await txb_adv
+	print_to_menu(text)
+	await txb_adv
 
 func print_to_menu(text):
-    # will b more sophisiticated later
-    clear_menu()
-    %MenuString.text = text
+	# will b more sophisiticated later
+	clear_menu()
+	%MenuString.text = text
 
 
 func take_damage(damage:int):
-    hp -= damage
-    pass
+	hp -= damage
+	pass
 
 func game_over():
-    #end the game
-    pass
+	#end the game
+	pass
 
 func credit_reached():
-    # kayla is sparable
-    pass
+	# kayla is sparable
+	pass
 
 func _input(event):
-    if event.is_action_pressed("ui_accept"):
-        txb_adv.emit()
-    elif event.is_action_pressed("ui_cancel"):
-        txb_back.emit()
-        pass
+	if event.is_action_pressed("ui_accept"):
+		txb_adv.emit()
+	elif event.is_action_pressed("ui_cancel"):
+		txb_back.emit()
+		pass
